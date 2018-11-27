86abbf4e235bfd854227c6e6af81ad683e6ffa0a
qy86134736 <1347360988@qq.com>
2018-11-27 21:57:57 +0800

    提交测试

---
+++ src/main/java/com/pro/two/com/pro/hotel/controller/test.java
@@ -0,0 +1,14 @@
+package com.pro.two.com.pro.hotel.controller;
+
+/**
+ * className:test
+ * discription:
+ * author:sjq
+ * createTime:2018-11-27 21:56
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hello workd");
+    }
+}
