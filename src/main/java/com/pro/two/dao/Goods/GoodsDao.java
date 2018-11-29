e8aed6b601852806e49a6149d7002731b217407f
aaaqy8608 <782250529@qq.com>
2018-11-29 13:51:25 +0800

    提交

---
+++ src/main/java/com/pro/two/dao/Goods/GoodsDao.java
@@ -0,0 +1,46 @@
+package com.pro.two.dao.Goods;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * className:GoodsDao
+ * discription:
+ * author:Ryb
+ * createTime:2018-11-23 16:47
+ */
+public interface GoodsDao {
+
+    /**
+     * 查询商品信息
+     * @param map
+     * @return
+     */
+    List<Map> getGoodsList(Map map);
+    /**
+     * 获取分页总数量
+     * @return
+     */
+    int getGoodsCount();
+
+    /**
+     * 商品添加
+     * @param map
+     * @return
+     */
+    int add(Map map);
+
+    /**
+     * 根据id删除goods
+     * @param id
+     * @return
+     */
+    int delete(Integer id);
+
+    /**
+     * 根据GOODS_ID修改商品信息
+     * @param map
+     * @return
+     */
+    int update(Map map);
+}
---
+++ src/main/java/com/pro/two/service/Goods/GoodsServiceImpl.java
@@ -0,0 +1,51 @@
+package com.pro.two.service.Goods;
+
+import com.pro.two.dao.Goods.GoodsDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * className:GoodsServiceImpl
+ * discription:
+ * author:Ryb
+ * createTime:2018-11-23 16:55
+ */
+@Service
+public class GoodsServiceImpl implements GoodsService {
+
+    @Autowired
+    private GoodsDao goodsDao;
+
+
+    @Override
+    public List<Map> getGoodsList(Map map) {
+        int pageNo = map.get("pageNo") == null ? 1 : Integer.valueOf(map.get("pageNo")+"");
+        int pageSize = map.get("pageSize") == null ? 10 : Integer.valueOf(map.get("pageSize")+"");
+        map.put("start",(pageNo-1)*pageSize);
+        map.put("end",pageNo*pageSize+1);
+        return goodsDao.getGoodsList(map);
+    }
+
+    @Override
+    public int getGoodsCount() {
+        return goodsDao.getGoodsCount();
+    }
+
+    @Override
+    public int add(Map map) {
+        return goodsDao.add(map);
+    }
+
+    @Override
+    public int delete(Integer id) {
+        return goodsDao.delete(id);
+    }
+
+    @Override
+    public int update(Map map) {
+        return goodsDao.update(map);
+    }
+}
